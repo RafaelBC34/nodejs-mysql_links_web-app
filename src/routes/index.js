a19573d20a2cbf2de7c317305735e747d13b5455
RafaelBC <rafaelbc34@gmail.com>
2022-10-19 18:54:40 +0200

    Funcionalidad principal terminada.

---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+const pool = require('../database')
+
+router.get('/', (req, res) => {
+    res.render('index')
+})
+
+module.exports = router
